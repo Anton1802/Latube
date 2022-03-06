8933cd2f52b52e11ee0a5b9f8db3be268307badd
Anton1802 <anton.sup1802@gmail.com>
2022-03-07 01:51:02 +0300

    create router and controllers

--- latube/resources/views/layouts/video.blade.php
+++ latube/resources/views/layouts/video.blade.php
@@ -8,7 +8,7 @@
   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Название видео</h5>
-    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p class="card-text">Lorem ipsum dolor sit amet</p>
     <a class="btn btn-primary btn-sm" href="#">Смотреть</a>
   </div>
 </div>
