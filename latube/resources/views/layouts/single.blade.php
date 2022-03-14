4cb5df45c58d93a2bf5851cba1965cec3b79717d
Anton1802 <anton.sup1802@gmail.com>
2022-03-14 06:52:38 +0300

    layout border down video

--- latube/resources/views/layouts/single.blade.php
+++ latube/resources/views/layouts/single.blade.php
@@ -5,10 +5,25 @@
 @endsection
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
 <h1>{{ $video->title }}</h1>
 <video id="player" playsinline controls>
     <source src="/video/{{ $video->id }}/stream" type="video/mp4" />
 </video>
+<div class="border border-dark mt-2 ps-2 pe-2 pt-2 rounded">
+    <div class="description">
+    <b>Описание:</b>
+    <p>{{ $video->description }}</p>
+    </div>
+    <div class="count-views">
+    <b>Количество просмотров:</b><p>{{$video->views}} просмотр(ов)</p>
+    </div>
+    <div class="author">
+    <b>Автор:</b> Anton
+    </div>
+    <div class="like text-end pb-2">
+    <a class="btn btn-danger">Нравится</a>
+    </div>
+</div>
 </div>
 @endsection
