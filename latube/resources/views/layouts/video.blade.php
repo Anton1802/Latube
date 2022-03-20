ff26afd36b5b560572374f52993b0fac8d5cf1dd
Anton1802 <anton.sup1802@gmail.com>
2022-03-21 00:53:08 +0300

    resize img

--- latube/resources/views/layouts/video.blade.php
+++ latube/resources/views/layouts/video.blade.php
@@ -10,10 +10,10 @@ Latube
     @foreach($videos as $video)
 <div class="video mt-5 col-auto ">
 <div class="card" style="width: 18rem;">
-  <img src="{{ $video->img_path }}" class="card-img-top">
+  <img src="{{ $video->img_path }}" class="card-img-top" style="height:180px;">
   <div class="card-body">
     <h5 class="card-title">{{ $video->title }}</h5>
-  
+
     <p>{{ $video->views }} просмотр(ов)</p>
     <a class="btn btn-primary btn-sm mt-2" href="video/{{ $video->id }}">Смотреть</a>
   </div>
