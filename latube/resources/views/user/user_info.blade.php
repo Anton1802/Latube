d6698441b96a514b23ca1909162b8ee8320f3b84
Anton1802 <anton.sup1802@gmail.com>
2022-03-26 15:20:21 +0300

    edit html

--- latube/resources/views/user/user_info.blade.php
+++ latube/resources/views/user/user_info.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="container">
-<div class="userinfo alert alert-primary mt-5">
+<div class="userinfo mt-5">
   <div class="row">
   <h4 class="text-center">Информация о вас</h4>
   <div class="avatar col-auto">
@@ -22,6 +22,21 @@
     <p><b>Дата регистрации:</b> {{ $user->created_at }}</p>
   </div>
   </div>
+</div>
+<h5 class="text-center">Мои видео</h5>
+<div class="videolist overflow-auto" style="max-height: 300px;">
+  <ul class="list-group">
+  @foreach($videos as $video)
+  <li class="list-group-item">
+  <div class="row">
+  <p>Название видео: {{ $video->title }}</p>
+  <p>Просмотры: {{ $video->views }}</p>
+  <p>Дата загрузки: {{ $video->updated_at }}</p>
+  <a href="/video/{{ $video->id }}" class="btn btn-primary ">Подробнее</a>
+  </div>
+  </li>
+  @endforeach
+</ul>
 </div>
 
 </div>
